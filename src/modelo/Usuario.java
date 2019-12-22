f08a09482eea397aa6ec07d4ab1d97d74ec91f97
LaniaMaria <laniambarros@gmail.com>
2019-12-22 20:32:39 -0300

    iniciado aplicacao para cadastro de filmes e usuario de uma locadora; implementado as classes Filme.class;Usuario.class;Locacao.class do pacote modelo.

---
+++ src/modelo/Usuario.java
@@ -0,0 +1,22 @@
+package modelo;
+
+public class Usuario {
+	
+	private String Nome;
+	
+	public Usuario() {}
+
+	public Usuario(String Nome) {
+		this.Nome = Nome;
+		
+	}
+
+	public String getNome() {
+		return Nome;
+	}
+
+	public void setNome(String Nome) {
+		this.Nome = Nome;
+	}
+}
+
